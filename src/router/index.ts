e0f0bdbec99a0647152aad88dabcd0a9e3c12589
Leonard <71197922+LeonardTK@users.noreply.github.com>
2021-06-08 18:29:52 +0000

    Update index.ts
    New routes.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,37 @@
-import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+import Pagenot from '../views/PageNotFound.vue'
+
 
 const routes: Array<RouteRecordRaw> = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-      },
-      {
-        path: '/login',
-        name: 'Login',
-        component: Login,
-      },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'PageNotFound',
+    component: Pagenot
+  },
 ];
 
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-  })
-  
+  history: createWebHistory(),
+  routes
+})
+
 export default router;
