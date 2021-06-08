3dc2d4f60cf8a2536102c75f4329d9311e49dec1
Leonard <71197922+LeonardTK@users.noreply.github.com>
2021-06-08 18:36:21 +0000

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="text-center my-3">
-    <h4>Rip.codes</h4>
     <div class="d-flex justify-content-center">
       <HeaderLink to="/" exact>Home</HeaderLink>
-      <Header-link to="/login" exact> Login</Header-link>
+      <HeaderLink to="/login" exact>Login</HeaderLink>
+      <HeaderLink to="/signup" exact>Signup</HeaderLink>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HeaderLink from "./HeaderLink.vue";
+import { defineComponent } from 'vue'
+import HeaderLink from './HeaderLink.vue'
 export default defineComponent({
-  components: { HeaderLink },
-});
+  components: { HeaderLink }
+})
 </script>
